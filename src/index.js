5c548db9c1c027bb6c9dec76cc186c2b3893b0a4
= <=>
2021-06-30 17:59:00 -0300

    buscar saque pela data

--- src/index.js
+++ src/index.js
@@ -13,25 +13,25 @@ const customers = []
  */
 
 // Middleware
-function verifyIfExistsAccountCPF(request, response, next){
+function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers
 
     const customer = customers.find((customer) => customer.cpf === cpf)
 
     if (!customer) {
-        return response.status(400).json({error: "Customer not found"})
+        return response.status(400).json({ error: "Customer not found" })
     }
     request.customer = customer
     return next()
 }
 
-app.post("/account", (request,response) =>{
-    const {cpf,name} = request.body
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body
     const customersAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf
     )
     if (customersAlreadyExists) {
-        return response.status(400).json({error: "Customer already exists!"})
+        return response.status(400).json({ error: "Customer already exists!" })
     }
 
     customers.push({
@@ -43,9 +43,34 @@ app.post("/account", (request,response) =>{
     return response.status(201).send()
 })
 
-app.get("/statement", verifyIfExistsAccountCPF, (request,response) =>{
-    const {customer} = request
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
     return response.json(customer.statement)
 })
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body
+
+    const { customer } = request
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    customer.statement.push(statementOperation)
+    return response.status(201).send()
+})
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    const { date } = request.query
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter(
+        (statement) => 
+        statement.created_at.toDateString() === 
+        new Date(dateFormat).toDateString())
+    return response.json(statement)
+})
 app.listen(3333)
